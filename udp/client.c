add92fbaaa13eed5282f6443cd02eb5d2ae8d1c7
Oussama Souidi <souidioussama14@gmail.com>
2025-11-23 15:18:38 +0100

    project done

--- udp/client.c
+++ udp/client.c
@@ -1,58 +1,67 @@
 #include <stdio.h>
-#include <stdbool.h>
-#include <strings.h>
-#include <sys/types.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <stdbool.h>
 
-#define PORT 5000 
 #define NMAX 100
-#define MAXLINE 1000
-int main() {
-char buffer[NMAX];
-int len;
-
-int fd = socket(AF_INET, SOCK_DGRAM, 0);
-if (fd < 0) {
-    perror("échec de création de socket");
-    return -1;     
-}  
-
-struct sockaddr_in addresse;
+#define BUFFER_SIZE 1000
+int main(int argc, char *argv[])
 {
-    addresse.sin_family = AF_INET;
-    addresse.sin_addr.s_addr = inet_addr("127.0.0.1");
-    addresse.sin_port = htons(PORT);
-};
-if(connect(fd , (struct sockaddr *)&addresse, sizeof(addresse)) < 0) {
-    perror("échec de la connexion");
-    exit(0);
-}
-printf("connexion établie avec le serveur \n");
-printf("Donner un entier entre 1 et 100: \n");
+    if (argc != 3)
+    {
+        printf("Usage: %s <hostname> <port>\n", argv[0]);
+        return 1;
+    }
 
-int n;
-int res = scanf("%d", &n);
-bool valid = n >= 1 && n <= NMAX;
+    char *hostname = argv[1];
+    int port = atoi(argv[2]);
 
-while(!valid) {
-    printf("Entrée invalide. Veuillez entrer un entier entre 1 et 100: \n");
-    while(getchar() != '\n');
+    char buffer[NMAX];
+    struct sockaddr_in addresse;
+    {
+        addresse.sin_family = AF_INET;
+        addresse.sin_addr.s_addr = inet_addr(hostname);
+        addresse.sin_port = htons(port);
+    };
+
+    int fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (fd < 0)
+    {
+        perror("échec de création de socket");
+        return -1;
+    }
+
+    if (connect(fd, (struct sockaddr *)&addresse, sizeof(addresse)) < 0)
+    {
+        perror("échec de la connexion");
+        exit(0);
+    }
+    printf("connexion établie avec le serveur \n");
+    printf("Donner un entier entre 1 et 100: \n");
+
+    int n;
     int res = scanf("%d", &n);
-    valid = n >= 1 && n <= NMAX;
-}
+    bool valid = n >= 1 && n <= NMAX;
 
+    while (!valid)
+    {
+        printf("Entrée invalide. Veuillez entrer un entier entre 1 et 100: \n");
+        while (getchar() != '\n')
+            ;
+        int res = scanf("%d", &n);
+        valid = n >= 1 && n <= NMAX;
+    }
 
-send(fd, &n, sizeof(n), 0);
-recvfrom(fd, buffer, sizeof(buffer), 0, (struct sockaddr*)NULL, NULL);
-int size = sizeof(&buffer);;
-//printf("%d", size);
-for(int i = 0; i < n; i++) {
-    printf("%d\n", ((int *)buffer)[i]);
-}
+    send(fd, &n, sizeof(n), 0);
+    recvfrom(fd, buffer, sizeof(buffer), 0, (struct sockaddr *)NULL, NULL);
+    printf("Les %d nombres reçus du serveur:\n", n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", ((int *)buffer)[i]);
+    }
 
-close(fd);
+    close(fd);
 }
--- udp/serveur.c
+++ udp/serveur.c
@@ -7,61 +7,72 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-#define PORT 5000 
 #define NMAX 100
-#define MAXLINE 1000
-int main() {
-char buffer[NMAX];
-int len;
-int list[NMAX];
+#define BUFFER_SIZE 1000
 
-
-int fd = socket(AF_INET, SOCK_DGRAM, 0);
-if (fd < 0) {
-    perror("échec de création de socket");
-    return -1;     
-}  
-
-struct sockaddr_in addresse, client;
+int main(int argc, char *argv[])
 {
-    addresse.sin_family = AF_INET;
-    addresse.sin_addr.s_addr = INADDR_ANY;
-    addresse.sin_port = htons(PORT);
-};
-if (bind(fd, (struct sockaddr *)&addresse, sizeof(addresse)) < 0) {
-    perror("échec du bind");
-    exit(0);    
-}
-while(1) {
-len = sizeof(client);
 
-int n = recvfrom(fd, buffer, sizeof(buffer), 0, (struct sockaddr *)&client, &len);
-if (n < 0) {
-    perror("échec du réception du message");
-    exit(0);    
-}
-/*buffer[n] = '\0';
-puts(buffer);
+    if (argc != 2)
+    {
+        printf("Usage: %s <port>\n", argv[0]);
+        return 1;
+    }
 
-uint32_t x = atoi(buffer);
-srand(time(NULL));
+    int port = atoi(argv[1]);
+    char buffer[BUFFER_SIZE];
 
-for (int i = 0; i < x; i++) {
-    int randomNumber = rand();
-    list[i] = randomNumber;
-}
-sendto(fd, &list, sizeof(list), 0, (struct sockaddr *)&client, len);
-*/
+    int fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (fd < 0)
+    {
+        perror("échec de création de socket");
+        return -1;
+    }
 
-printf("Nombre reçu du client: %d\n", *(int *)buffer);
-int x = *(int *)buffer;
-srand(time(NULL));
-//x = rand() % 100 + 1;
-//sendto(fd, &x, sizeof(x), 0, (struct sockaddr *)&client, len);
-int list[NMAX];
-for (int i = 0; i < x; i++) {
-    int randomNumber = rand()% 1000 + 1;
-    list[i] = randomNumber;
+    struct sockaddr_in serveur, client;
+    {
+        serveur.sin_family = AF_INET;
+        serveur.sin_addr.s_addr = INADDR_ANY;
+        serveur.sin_port = htons(port);
+    };
+
+    if (bind(fd, (struct sockaddr *)&serveur, sizeof(serveur)) < 0)
+    {
+        perror("échec du bind");
+        return -1;
+    }
+    printf("Serveur démarré sur le port %d\n", port);
+
+    while (1)
+    {
+        int len = sizeof(client);
+
+        if (recvfrom(fd, buffer, sizeof(buffer), 0, (struct sockaddr *)&client, &len) < 0)
+        {
+            perror("échec du réception du message");
+            return -1;
+        }
+        printf("Client connecté: %s:%d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+        printf("Nombre reçu du client: %d\n", *(int *)buffer);
+
+        int x = *(int *)buffer;
+        srand(time(NULL));
+
+        int list[NMAX];
+        for (int i = 0; i < x; i++)
+        {
+            int randomNumber = rand() % 1000 + 1;
+            list[i] = randomNumber;
+        }
+
+        printf("Les %d nombres envoyés au client: ", x);
+        for (int i = 0; i < x; i++)
+        {
+            printf("%d ", list[i]);
+        }
+        printf("\n");
+        
+        sendto(fd, &list, sizeof(list), 0, (struct sockaddr *)&client, len);
+    }
+    return 0;
 }
-sendto(fd, &list, sizeof(list), 0, (struct sockaddr *)&client, len);
-}}
